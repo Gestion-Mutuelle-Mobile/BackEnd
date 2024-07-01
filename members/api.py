c4188367dabd7c5c10afd53afc719cd6a2fd1356
MAG-ENCRYPTION <mbogerard03@gmail.com>
2024-07-01 15:01:54 +0100

    New Backend

--- members/api.py
+++ members/api.py
@@ -33,22 +33,3 @@ class UnpaidObligatoryContributionMembersViewSet(viewsets.ViewSet):
         # Sérialiser les membres
         serializer = MemberSerializer(unpaid_members, many=True)
         return Response(serializer.data)
-
-class Helped(viewsets.ViewSet):
-    #permission_classes = [IsAuthenticated]
-
-    def list(self, request):
-        """
-        Retourne la liste des membres qui n'ont pas encore payé leur contribution obligatoire pour toutes les sessions.
-        """
-        # Obtenir les membres qui n'ont pas encore payé leur contribution pour toutes les sessions
-        unpaid_members = Member.objects.filter(
-            obligatory_contribution__contributed=0
-    
-        ).distinct()
-
-        helped_members = Member.objects.filter().distinct()
-
-        # Sérialiser les membres
-        serializer = MemberSerializer(unpaid_members, many=True)
-        return Response(serializer.data)
