4f0324d8d573d15cd8616afd8dadb4bd4cdce86d
Tiger-Foxx <donfackarthur750@gmail.com>
2024-11-14 13:36:06 +0100

    test

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,11 @@
 from rest_framework import routers
 from .api import UserViewSet
-
+from django.urls import path
+from .views import VerifyPasswordView
 router = routers.DefaultRouter()
 router.register('api/users',UserViewSet,'users')
-urlpatterns = router.urls
+urlpatterns = [
+    path('verify-password/', VerifyPasswordView.as_view(), name='verify_password'),
+]+router.urls
+
+#com
--- users/views.py
+++ users/views.py
@@ -2,3 +2,26 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import check_password
+
+User = get_user_model()
+
+class VerifyPasswordView(APIView):
+    def post(self, request):
+        user_id = request.data.get("user_id")
+        password = request.data.get("password")
+
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        # Vérification du mot de passe en utilisant `check_password`
+        if check_password(password, user.password):
+            return Response({"status": "success"}, status=status.HTTP_200_OK)
+        else:
+            return Response({"status": "failure"}, status=status.HTTP_401_UNAUTHORIZED)
