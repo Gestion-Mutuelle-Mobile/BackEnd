84ed0a93445680e70e8f1613e2efdeaee359b531
Tiger-Foxx <donfackarthur750@gmail.com>
2024-11-14 02:00:51 +0100

    ajout de la page d'administration

--- administrators/admin.py
+++ administrators/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from administrators.models import Administrator
+
+
 # Register your models here.
+@admin.register(Administrator)
+class AdministratorAdmin(admin.ModelAdmin):
+    pass
